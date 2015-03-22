980709efaf9ad8987b973071abfb199bba591e9f
Órion Winter <orion.lima@ccc.ufcg.edu.br>
2015-03-22 01:20:39 -0300

    Controller de meta dica terminado

--- app/controllers/MetaDicas.java
+++ app/controllers/MetaDicas.java
@@ -3,6 +3,7 @@ package controllers;
 import models.dao.GenericDAO;
 import models.dica.DicaGenerica;
 import models.dica.MetaDica;
+import play.Logger;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.db.jpa.Transactional;
@@ -11,6 +12,7 @@ import views.html.metadicas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static play.mvc.Controller.flash;
 import static play.mvc.Controller.session;
@@ -28,32 +30,36 @@ public class MetaDicas {
         DynamicForm filledForm = Form.form().bindFromRequest();
         if (filledForm.hasErrors()){
             flash("message", "Formulário invalido!");
-            metaDica();
+            show();
         }
         String comentario = filledForm.get("comentario");
+        Map<String, String> form = filledForm.data();
+        List<DicaGenerica> dicas = new ArrayList<DicaGenerica>();
+
+        for (int i = 0; i < form.size() - 1; i++){
+            dicas.add(dao.findByEntityId(DicaGenerica.class, Long.parseLong(form.get("dica["+i+"]"))));
+        }
+
         try {
-            List<DicaGenerica> dicas = new ArrayList<DicaGenerica>();
             DicaGenerica metaDica = new MetaDica(session().get("user"), comentario, dicas);
             dao.persist(metaDica);
-            return metaDica();
+            return show();
         } catch (Exception e) {
             flash("message", e.getMessage());
-            return metaDica();
+            return show();
         }
     }
 
     @Transactional
-    public static Result metaDica() {
+    public static Result show() {
         String message = flash("message");
         List<DicaGenerica> dicas = dao.findAllByClass(DicaGenerica.class);
         List<DicaGenerica> metaDicas = new ArrayList<DicaGenerica>();
-
         for (DicaGenerica dica: dicas){
-            if (dica instanceof DicaGenerica){
+            if (dica instanceof MetaDica){
                 metaDicas.add(dica);
             }
         }
-
         return ok(metadicas.render(message, dicas, metaDicas));
     }
 
